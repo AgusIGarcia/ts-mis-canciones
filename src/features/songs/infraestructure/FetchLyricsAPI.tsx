a0df9a805c744df08264f885d80c98ed4291609c
agustin.garcia <agustin.garcia@diveria.com>
2022-08-04 10:10:40 -0300

    Error lyrics API

--- src/features/songs/infraestructure/FetchLyricsAPI.tsx
+++ src/features/songs/infraestructure/FetchLyricsAPI.tsx
@@ -3,12 +3,12 @@ import { ILyricsAPI } from "../domain/ILyricsAPI";
 export class FetchLyricsAPI implements ILyricsAPI {
   private _url = process.env.REACT_APP_LYRICS_API_URL;
   async get(artistName: string, songName: string): Promise<string> {
-    let songAPI = this._url + artistName + "/" + songName,
-      songRes = await fetch(songAPI),
-      songJSON = await songRes.json();
-    if(songJSON.lyrics === undefined){
-      throw new Error("Lyrics not found");
-    }
-    return songJSON.lyrics;
+    // let songAPI = this._url + "artist="+ artistName + "&song=" + songName,
+    //   songRes = await fetch(songAPI, options),
+    //   songJSON = await songRes.json();
+    // if(songJSON.lyrics === undefined){
+    //   throw new Error("Lyrics not found");
+    // }
+    return "Se rompió la API";
   }
 }
